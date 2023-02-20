382ed6eb0690277fb011a1f87db450fad526d3ce
Clément Pons <clement.pons@stms.fr>
2023-02-20 17:52:43 +0100

    First interface tests for modal and caching.

--- main.ts
+++ main.ts
@@ -1,85 +1,83 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface CharacterBuilderSettings {
+	folder: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: CharacterBuilderSettings = {
+	folder: 'Characters'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class CharacterBuilderCache {
+	static _cache = {};
+	static cache(path: string, value: string|number|undefined)
+	{
+		if(!!value)
+			return CharacterBuilderCache.write_cache(path, value);
+		else
+			return CharacterBuilderCache.read_cache(path);
+	}
+	private static read_cache(path: string)
+	{
+		const keys = path.split("/");
+		let value = CharacterBuilderCache._cache;
+		for(let i = 0; i < keys.length; i++)
+		{
+			if(value.hasOwnProperty(keys[i]))
+				value = value[keys[i]];
+			else
+				return undefined;
+		}
+		return value;
+	}
+	private static write_cache(path: string, value: string|number)
+	{
+		const keys = path.split("/");
+		let val = CharacterBuilderCache._cache;
+		for(let i = 0; i < keys.length - 1; i++)
+		{
+			if(val.hasOwnProperty(keys[i]))
+				val = val[keys[i]];
+			else
+				val = val[keys[i]] = {};
+		}
+		val[keys[keys.length - 1]] = value;
+
+		console.log(CharacterBuilderCache._cache)
+	}
+}
+
+const characterBuilderCache = new CharacterBuilderCache();
+
+export default class CharacterBuilder extends Plugin {
+	settings: CharacterBuilderSettings;
 
 	async onload() {
 		await this.loadSettings();
+		await this.initCache();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('calculator', 'Create new character', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			new CharacterBuilderModal(this.app).open();
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.addSettingTab(new CharacterBuilderSettingTab(this.app, this));
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		//this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
+	}
+
+	async initCache() {
+		const files = this.app.vault.getMarkdownFiles();
+
+		CharacterBuilderCache.cache("names/races", files.filter(e => e.path.includes("Liste des Races")).map(e => e.basename));
+		CharacterBuilderCache.cache("names/talents", files.filter(e => e.path.includes("2. Talents")).map(e => e.basename));
 	}
 
 	async loadSettings() {
@@ -91,26 +89,53 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+class CharacterBuilderModal extends Modal {
+	name: string;
+	race: string;
+	subrace: string;
+	feature: string;
+
 	constructor(app: App) {
 		super(app);
 	}
 
 	onOpen() {
+		let raceDropdown, subraceDropdown, featureDropdown;
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+
+		contentEl.createEl("h2", { text: "Character creation" });
+
+		new Setting(contentEl)
+			.setName("Character Name")
+			.addText(text => text.onChange(value => this.name = value));
+
+		contentEl.createEl("h4", { text: "Character's Race" });
+
+		raceDropdown = new Setting(contentEl)
+			.setName("Race")
+			.addDropdown(dropdown => 
+				dropdown.addOptions(CharacterBuilderCache.cache("names/races").forEach(e => dropdown.addOption(e, e)))
+						.onChange(value => { this.race = value; }));
+
+		subraceDropdown = new Setting(contentEl)
+			.setName("Sub-Race")
+			.addText(text => text.onChange(value => this.subrace = value));
+
+		featureDropdown = new Setting(contentEl)
+			.setName("Race Feature")
+			.addText(text => text.onChange(value => this.feature = value));
+
+		new Setting(contentEl)
+			.addButton(btn => btn.setButtonText('Create')/*.setIcon('plus-circle')*/.onClick(console.log));
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+	listen()
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class CharacterBuilderSettingTab extends PluginSettingTab {
+	plugin: CharacterBuilder;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: CharacterBuilder) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,17 +145,15 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for character builder.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Character Folder')
+			.setDesc('Link to the character folder')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.folder)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.character = value;
 					await this.plugin.saveSettings();
 				}));
 	}
